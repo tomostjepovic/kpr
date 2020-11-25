8f9668d9cfc055073c42156b1597e4db61136cdc
tstjepovic_polleosport <tstjepovic@polleosport.com>
2020-11-25 21:42:39 +0100

    Init layout

---
+++ src/web/scripts/app.js
@@ -0,0 +1,9 @@
+function test() {
+    alert('Unesite dob!');
+}
+document.getElementById('Starije').onclick = test; 
+
+$("#weight" ).click(function() {
+    alert( "Handler for .click() called." );
+});
+
